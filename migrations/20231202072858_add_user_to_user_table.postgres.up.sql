27db1ca99cc7c6fed2b0a827c9fc5d0e34aae5ee
Jay Zhou <jzilla808@gmail.com>
2023-12-03 01:27:29 -0800

    added database migration for initial admin user

---
+++ migrations/20231202072858_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Jay',E'Zhou',E'admin@admin.com',E'$2a$12$NjHHtAei9ikqdPiObWYC4eO8i4f3cpvnkn.6Sg1V/XapbXUfx/MmO',3,E'2020-12-04 00:00:00',E'2020-12-04 00:00:00');
